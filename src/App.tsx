ed24ebe8677bac2f2812a01044d53aa07db39981
Liam Slagle <lslagle@taivara.com>
2023-03-08 23:00:26 -0500

    Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,7 @@
 import React from 'react';
 
 function App() {
-  const a = [
-    'askjdasjkd',
-    'sdjhflasdkjfhas',
-    'dhflaksjdhfkljsh',
-    'kdjhlksdjhlsdjhfsd',
-  ];
-  console.log(a);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reloadd.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
