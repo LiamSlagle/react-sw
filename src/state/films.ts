d76eb05809d2ed82fb6f2eae1ec0cfa287b7b9eb
Liam Slagle <lslagle@taivara.com>
2023-03-13 12:47:24 -0400

    Add hash map for film roman nums and selector to select film by ID

--- src/state/films.ts
+++ src/state/films.ts
@@ -1,6 +1,16 @@
 import { createSlice, createEntityAdapter } from '@reduxjs/toolkit';
 import { RootState } from './store';
 
+// Look into recursively converting int to roman numeral if api is expanded
+export const ROMAN_NUMS: { [key: number]: string } = {
+  1: 'I',
+  2: 'II',
+  3: 'III',
+  4: 'IV',
+  5: 'V',
+  6: 'VI',
+};
+
 export interface Film {
   episode_id: number;
   title: string;
@@ -33,9 +43,8 @@ const filmsSlice = createSlice({
 });
 
 // Destructure selectAll so we can import directly later
-export const { selectAll: selectFilms } = filmsAdapter.getSelectors<RootState>(
-  (state) => state.films,
-);
+export const { selectAll: selectFilms, selectById: selectFilmById } =
+  filmsAdapter.getSelectors<RootState>((state) => state.films);
 
 const { actions, reducer } = filmsSlice;
 export const { mergeFilms, resetFilms } = actions;
